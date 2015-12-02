af62549e1515d6fbaa097464c95879a4b3feec3a
kritner <kritner@gmail.com>
2015-12-02 10:25:49 -0500

    Updated Business.Wcf.Service1 to work off of interface rather than concrete. added unit tests for Service1.  added runsettings file to exclude "Service References" folders from code coverage.  Fix #2

---
+++ RussUnitTestSample.Business.Tests/WCF/Service1Tests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RussUnitTestSample.Business.ServiceReference1;
+
+namespace RussUnitTestSample.Business.Tests.WCF
+{
+
+    /// <summary>
+    /// Unit tests for Service1
+    /// </summary>
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class Service1Tests
+    {
+
+        #region Private
+        private Mock<IService1> _service;
+        #endregion Private
+
+        #region Public methods
+        /// <summary>
+        /// initialize the mocks
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            this._service = new Mock<IService1>();
+        }
+
+        /// <summary>
+        /// Exception thrown when IService implementation is not provided
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Service1_NullIService1InConstructor_ThrowsException()
+        {
+            // Arrange / Act
+            Business.WCF.Service1 service = new Business.WCF.Service1(null);
+        }
+
+        /// <summary>
+        /// Object properly constructed when implementation of IService1 provided
+        /// </summary>
+        [TestMethod]
+        public void Service1_ConstructorWithProvidedIService1_NewsCorrectly()
+        {
+            // Arrange / Act
+            Business.WCF.Service1 service = new Business.WCF.Service1(_service.Object);
+
+            // Assert
+            Assert.IsInstanceOfType(service, typeof(Business.WCF.Service1));
+        }
+
+        /// <summary>
+        /// Ensure that a string is returned from Service1 when calling GetData
+        /// </summary>
+        [TestMethod]
+        public void Service1_GetDataTest()
+        {
+            // Arrange
+            this._service.Setup(s => s.GetData(It.IsAny<int>())).Returns("test");
+            Business.WCF.Service1 service = new Business.WCF.Service1(_service.Object);
+
+            // Act
+            var result = service.GetData(It.IsAny<int>());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        #endregion Public methods
+
+    }
+}
--- RussUnitTestSample.Business/WCF/Service1.cs
+++ RussUnitTestSample.Business/WCF/Service1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using RussUnitTestSample.Business.ServiceReference1;
 
 namespace RussUnitTestSample.Business.WCF
 {
@@ -14,18 +16,46 @@ namespace RussUnitTestSample.Business.WCF
     {
 
         #region Private
-        private ServiceReference1.Service1Client _service;
+        private IService1 _service;
         #endregion Private
 
+        #region ctor
+
+        /// <summary>
+        /// Constructor - new up IService1 with client
+        /// </summary>
         public Service1()
         {
-            this._service = new ServiceReference1.Service1Client();
+            this._service = new Service1Client();
         }
 
+        /// <summary>
+        /// Constructor - takes in implementation of IService1
+        /// </summary>
+        /// <param name="service">The IService1 implementation</param>
+        public Service1(IService1 service)
+        {
+            if (service == null)
+                throw new ArgumentNullException(nameof(service));
+
+            this._service = service;
+        }
+
+        #endregion ctor
+
+        #region Public methods
+
+        /// <summary>
+        /// Call service GetData
+        /// </summary>
+        /// <param name="value">The value to pass to the WCF service</param>
+        /// <returns>The returned value from the WCF service call</returns>
         public string GetData(int value)
         {
             return this._service.GetData(value);
         }
+
+        #endregion Public methods
 
     }
 }
