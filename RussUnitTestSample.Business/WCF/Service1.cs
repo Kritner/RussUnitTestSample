e8458cbedb146040728cca7202f76503704862e5
kritner <kritner@gmail.com>
2015-12-02 07:29:00 -0500

    Files missed in last commit

---
+++ RussUnitTestSample.Business/WCF/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussUnitTestSample.Business.WCF
+{
+
+    /// <summary>
+    /// Communication with the WCF Service1
+    /// </summary>
+    public class Service1
+    {
+
+        #region Private
+        private ServiceReference1.Service1Client _service;
+        #endregion Private
+
+        public Service1()
+        {
+            this._service = new ServiceReference1.Service1Client();
+        }
+
+        public string GetData(int value)
+        {
+            return this._service.GetData(value);
+        }
+
+    }
+}
